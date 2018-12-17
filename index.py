68b73e8f330b1d42afa2379539417f4a04d6428c
rkatanguri <revanthreddy.katanguri@oubt.org>
2018-12-16 23:21:58 -0800

    1. FUNCTION FOR STORING 'USERINFO' -DONE 2. ROUTE FOR 'ORDER_SUMMARY ADDED- NEEDED TO CONFIRM 3. FUNCTIONS FOR GENERATING  'ORDER_ID', 'USER_ID' - DONE 4. HAVE TO DO THE DYNAMIC WAY OF INSERTING THE ORDERIDS, PRODUCTIDS, QUANTITY

--- index.py
+++ index.py
@@ -23,27 +23,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# @app.route('/get_movie_detail', methods=['POST'])
-# def get_movie_detail():
-#     data = request.get_json(silent=True)
-#     movie = data['queryResult']['parameters']['movie']
-#     api_key = os.getenv('OMDB_API_KEY')
-#
-#     movie_detail = requests.get('http://www.omdbapi.com/?t={0}&apikey={1}'.format(movie, api_key)).content
-#     movie_detail = json.loads(movie_detail)
-#     response =  """
-#         Title : {0}
-#         Released: {1}
-#         Actors: {2}
-#         Plot: {3}
-#     """.format(movie_detail['Title'], movie_detail['Released'], movie_detail['Actors'], movie_detail['Plot'])
-#
-#     reply = {
-#         "fulfillmentText": response,
-#     }
-#
-#     return jsonify(reply)
-
 
 @app.route('/get_product_detail', methods=['POST'])
 def get_product_detail():
